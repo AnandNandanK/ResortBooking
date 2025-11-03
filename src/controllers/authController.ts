94d3f0814ceff7d67033a0c7aba48393da7435c5
AnandNandanK <anandwising@gmail.com>
2025-11-03 09:14:45 +0530

    testing complete

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -209,13 +209,13 @@ export const loginUser = asyncHandler(async (req: Request, res: Response) => {
     { expiresIn: "7d" }
   );
 
-
   // Set token in HTTP-only cookie
   res.cookie("token", token, {
     httpOnly: true,        // makes cookie inaccessible to JS
     secure: process.env.NODE_ENV === "production", // true on HTTPS
     sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    //  maxAge: 30 * 1000,  
   });
 
   // Send response
